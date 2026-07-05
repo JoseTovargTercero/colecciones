42a1be703f49fbd8126927cf52339c6fc098c5c0
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2026-07-05 10:16:07 -0400

    asdad

--- config/Database.php
+++ config/Database.php
@@ -17,7 +17,7 @@ class Database
         }
         // =======================
 
-        $dir = APP_ROOT . '../../.env_colecciones';
+        $dir = APP_ROOT . '../../env_colecciones';
         $this->loadEnv($dir);
 
 
